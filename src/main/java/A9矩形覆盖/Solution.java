7fd21bc5c80361fdfb76756336c6353376cefaa2
Plumblumpb <2573350997@qq.com>
2018-11-02 17:59:00 +0800

    1.提交算法练习

--- src/main/java/A9矩形覆盖/Solution.java
+++ src/main/java/A9矩形覆盖/Solution.java
@@ -7,7 +7,11 @@ package A9矩形覆盖;
  */
 public class Solution {
     public int RectCover(int target) {
-
+      if(target == 1)
+          return 1;
+      if (target == 2)
+          return 2;
+      return 0;
     }
     public static void main(String[] args) {
 
